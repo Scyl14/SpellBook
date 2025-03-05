c3f08f5e4b11197818bee2fb35050a67a9638c24
Scyll4 <91890888+Scyl14@users.noreply.github.com>
2025-03-05 18:05:55 +0100

    Delete local_payload_fetch.py

--- local_payload_fetch.py
+++
@@ -1,6 +0,0 @@
-def local_payload_fetch():
-    path = input ("Enter Payload Path: ")
-    with open(path, "rb") as file:
-        content = file.read()
-        print ("unsigned char Data_RawData[] = {" + ", ".join(map(lambda b: hex(b), content)) + "};")
-
